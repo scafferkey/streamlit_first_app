2d491b38cce2d2ceed0bc09adc2b5251865f7f9e
scafferkey <51058061+scafferkey@users.noreply.github.com>
2022-03-21 13:55:50 +0000

    bar chart

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import plotly.express as px
 print('Hello World')
 
 st.title('Hello World? Title Mode')
@@ -7,3 +8,8 @@ st.write('This goes below')
 a = st.slider('Select a value')
 b = st.slider('Select another value')
 st.write(f'multiplying these two bad boys together gives us {a*b}')
+
+px.bar(x=['a', 'b'], y=[a, b])
+#%%
+
+#%%
