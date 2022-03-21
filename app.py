c38975f1b65e48cda7a9c234a5fda08922c6dba4
scafferkey <51058061+scafferkey@users.noreply.github.com>
2022-03-21 13:48:34 +0000

    sliders and text

--- app.py
+++ app.py
@@ -3,3 +3,7 @@ print('Hello World')
 
 st.title('Hello World? Title Mode')
 st.write('This goes below')
+
+a = st.slider('Select a value')
+b = st.slider('Select another value')
+st.write(f'multiplying these two bad boys together gives us {a*b}')
