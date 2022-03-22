6222f4cfc8df24fe7b32a80b59d097471e3e0103
scafferkey <51058061+scafferkey@users.noreply.github.com>
2022-03-22 10:11:01 +0000

    added a whole bunch of stuff

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.preprocessing import OrdinalEncoder
 
 print('Hello World')
 
@@ -12,12 +14,33 @@ st.write('This goes below')
 
 a = st.slider('Select a value')
 b = st.slider('Select another value')
-st.write(f'multiplying these two bad boys together gives us {a*b}')
+st.write(f'multiplying these two bad boys together gives us {a * b}')
 
-ab = pd.read_csv('https://raw.githubusercontent.com/jeaggo/tc3068/master/Superstore.csv')
-st.write(ab.head())
 
-sns.hisplot(ab['Quantity'])
-plt.show
+@st.experimental_memo
+def load_superstore():
+    superstore_dud = pd.read_csv('https://raw.githubusercontent.com/jeaggo/tc3068/master/Superstore.csv')
+    return superstore_dud
 
-#%%
+
+superstore = load_superstore()
+st.write(superstore.head())
+fig, ax = plt.subplots()
+ax = sns.histplot(superstore['Quantity'])
+st.pyplot(fig)
+print(superstore.columns)
+superstore.dropna(inplace=True)
+X = superstore[['Ship Mode', 'Segment', 'Country', 'City', 'State',
+                'Postal Code', 'Region', 'Product ID', 'Category', 'Sub-Category',
+                'Product Name', 'Sales', 'Quantity']]
+
+oe = OrdinalEncoder()
+X = oe.fit_transform(X=X)
+
+y = superstore['Profit']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+rf = RandomForestRegressor(random_state=35)
+rf.fit(X_train, y_train)
+print(rf.score(X_train, y_train))
+print(rf.score(X_test, y_test))
